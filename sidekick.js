65f981a42965072e14fafb2348a4014e6d224da0
Sinan Ascioglu <sinan@openprocessing.org>
2021-05-10 18:46:10 -0400

    initial commit

---
+++ sidekick.js
@@ -0,0 +1,27 @@
+let OpenProcessing = {
+	'requestDeviceMotion': function () {
+		return new Promise((resolve, reject) => {
+			OpenProcessing.deviceMotionAuthorized = function (data) {
+				if (data) {
+					resolve(data);
+				} else {
+					reject();
+				}
+			}
+			window.parent.postMessage({	'messageType': 'requestDeviceMotion'}, '*');
+		})
+	},
+	'requestUserInfo': function (requests = [], reason = '') {
+		return new Promise((resolve, reject) => {
+			OpenProcessing.userAuthorized = function (data) {
+				if (data) {
+					resolve(data);
+				} else {
+					reject();
+				}
+			}
+			window.parent.postMessage({ 'messageType': 'authUser', 'message': { 'requests': requests, 'reason': reason } }, '*');
+
+		});
+	}
+};
